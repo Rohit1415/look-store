6ed2db0ef8aa66f6e290f6fd5415caa4fb12d14c
Rohit Bhatu <bhaturohit549@gmail.com>
2023-11-19 16:14:49 +0530

    Appbar done✅

--- src/Component/Appbar.tsx
+++ src/Component/Appbar.tsx
@@ -10,7 +10,9 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
+import { InputAdornment, TextField } from '@mui/material';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import SearchIcon from '@mui/icons-material/Search';
 
 const pages = [
   {
@@ -26,7 +28,7 @@ const pages = [
     path: '/about',
   },
   {
-    title: 'Sign Up',
+    title: 'Profile',
     path: '/signup',
   },
 ];
@@ -55,14 +57,14 @@ const Appbar = () =>  {
     <AppBar position="static" sx={{p:1, bgcolor:"#fff", color:"#000", boxShadow:"0"}}>
         <Toolbar disableGutters>
           <Typography variant="h6" noWrap component="a" href="/" sx={{ mr: 2, display: { xs: 'none', md: 'flex' }, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }} >
-            EXCLUSIVE STORE
+            EXCLUSIVE
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit" >
               <MenuIcon />
             </IconButton>
-            <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left', }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'left', }} open={Boolean(anchorElNav)} onClose={handleCloseNavMenu} sx={{ display: { xs: 'block', md: 'none' }, }} >
+            <Menu id="menu-appbar" anchorEl={anchorElNav} anchorOrigin={{ vertical: 'bottom', horizontal: 'left', }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'left', }} open={Boolean(anchorElNav)} onClose={handleCloseNavMenu} sx={{ display: { xs: 'block', md: 'none' },textTransform:"capitalize" }} >
               {pages.map((page, index) => (
                 <MenuItem key={index} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">{page.title}</Typography>
@@ -71,33 +73,32 @@ const Appbar = () =>  {
             </Menu>
           </Box>
           <Typography textAlign='center' noWrap component="a" href="/" sx={{ mr: 2, display: { xs: 'flex', md: 'none' }, flexGrow: 1, fontFamily: 'monospace', fontWeight: 700, letterSpacing: '.3rem', color: 'inherit', textDecoration: 'none', }} >
-            EXCLUSIVE <br /> STORE
+            EXCLUSIVE 
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent:"end" }}>
+
+          {/* NOT RESPOSIVE BAR ITEMS */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {
               pages.map((page, index) => (
-                <Button key={index} onClick={handleCloseNavMenu} sx={{ my: 2, color: '#000', display: 'block', mr:1, fontSize:"19px", }} > 
+                <Button key={index} onClick={handleCloseNavMenu} sx={{ my: 2, color: '#000', display: 'block', mr:1, fontSize:"19px",textTransform:"capitalize" }} > 
                   {page.title}
                 </Button> ))
             }
           </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{ vertical: 'top', horizontal: 'right', }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right', }} open={Boolean(anchorElUser)} onClose={handleCloseUserMenu} >
-              {
-                settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))
-              };
-            </Menu>
+          <Box>
+            <TextField
+              placeholder='What are you looking for?'
+              variant="outlined"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
           </Box>
+         
         </Toolbar>
     </AppBar>
   );
